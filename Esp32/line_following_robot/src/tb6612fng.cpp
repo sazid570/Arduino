ff3ab53bf4b658a2eebf69102ec0c6c14ec0f3fa
Sazid Hasan <sazidhasan570@gmail.com>
2025-07-05 00:16:57 +0600

    hudai motor ghurailam

--- Esp32/line_following_robot/src/tb6612fng.cpp
+++ Esp32/line_following_robot/src/tb6612fng.cpp
@@ -1,12 +1,34 @@
-#include <Arduino.h>
+#include<Arduino.h>
+
+#define pwma 32
+#define pwmb 14
+#define motor1a 25
+#define motor2a 26
+#define motor1b 33
+#define motor2b 27
+
+
+
 
 void setup(){
     Serial.begin(115200);
-    Serial.println("TB6612FNG Motor Driver Test");
+    Serial.println("Tb6612fng motor driver test");
+    // pinMode(pwma,OUTPUT);
+    // pinMode(pwmb,OUTPUT);
+    pinMode(27,OUTPUT);
+    pinMode(26,OUTPUT);
+    
+
 }
 
 void loop(){
-    Serial.println("Hello world");
-    delay(1000); 
+    digitalWrite(27, HIGH);
+    digitalWrite(26, LOW);
+    Serial.println("Motor 1 ON");
+    delay(2000);
+    digitalWrite(27, LOW);
+    digitalWrite(26, HIGH);  
+    Serial.println("Motor 1 OFF");
+    delay(2000);
 
 }
