53f9788997d9d5a2eb057e222033835f5fda3517
Sazid Hasan <sazidhasan570@gmail.com>
2025-09-09 12:51:00 +0600

    rick roll

--- Arduino_micro/Micro_pro_hid/src/main.cpp
+++ Arduino_micro/Micro_pro_hid/src/main.cpp
@@ -2,12 +2,41 @@
 
 #include <Keyboard.h>
 
+void openCmd() {
+  // Press Windows key + R to open Run dialog
+  Keyboard.press(KEY_LEFT_GUI);  // Windows key
+  Keyboard.press('r');
+  delay(100);
+  Keyboard.releaseAll();
+  
+  // Wait for Run dialog to open
+  delay(500);
+  
+  // Type "cmd"
+  Keyboard.print("cmd");
+  
+  // Press Enter to execute
+  delay(200);
+  Keyboard.press(KEY_RETURN);
+  delay(100);
+  Keyboard.release(KEY_RETURN);
+
+  delay(500);
+
+  Keyboard.print("start https://tinyurl.com/rn8vb7dm");
+  Keyboard.press(KEY_RETURN);
+  delay(100);
+  Keyboard.release(KEY_RETURN);
+}
+
 void setup() {
-  delay(2000);  // Short delay to ensure host detects the device
+
   Keyboard.begin();
+  delay(1000);
+  openCmd();
 }
 
 void loop() {
-  Keyboard.println("Hello, world!");
-  delay(5000);  // Wait 5 seconds
+  // Your main code here
 }
+
---
+++ Esp32/line_following_robot/README.md
@@ -0,0 +1,52 @@
+# tb6612FNG motor driver
+
+
+
+
+
+ESP pin		Motor Driver
+
+-------		------------
+
+d4 	-> 	pwmA
+
+d18 	-> 	motor1 trigger B
+
+d19 	-> 	motor 1 trigger A
+
+5v  	-> 	STBY
+
+d21 	-> 	Motor2 Trigger A
+
+d22 	-> 	Motor2 Trigger B
+
+d23 	-> 	pwmB
+
+
+
+# Qrt8RC 8 array 
+
+
+
+ESP pin		Qrt8RC 8 array
+
+-------		--------------
+
+D13      ->	d8
+
+D14	 ->	d7
+
+D27	 ->	d6
+
+D26	 ->	d5
+
+D25	 ->	d4
+
+D33	 ->	d3
+
+D32	 ->	d2
+
+D35	 ->	d1
+
+
+
